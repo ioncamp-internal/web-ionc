bacc03880f7f3a3fb96e648687526acf8d5b5d4f
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-04-09 21:13:57 +0800

    Fix Zindex problem

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ export default function Example() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
     return (
-        <header className="bg-gray-900 z-10">
+        <header className="relative w-full bg-gray-900 z-10">
             <nav className="z-50 mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
                 <div className="flex lg:flex-1">
                     <a href="#" className="-m-1.5 p-1.5">
@@ -49,8 +49,8 @@ export default function Example() {
                 </div>
             </nav>
             <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-10" />
-                <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+                <div className="fixed inset-0 z-40" />
+                <Dialog.Panel className="fixed inset-y-0 right-0 z-40 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
                     <div className="flex items-center justify-between">
                         <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
