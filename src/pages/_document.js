6c4bc4345ece0d7ed933f57b5615609a49be4260
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-04-10 00:47:58 +0800

    Fix favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 import Header from '../components/Header'
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="zh">
+        <Head />
       <body>
         <Main />
         <NextScript />
