ac570b673eb09774397a5139cbcbb653dbed2110
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2023-05-03 23:46:32 +0800

    track

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import {clarity} from 'react-microsoft-clarity';
+import ReactGA from 'react-ga';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}) {
+    if (typeof window !== "undefined") {
+        clarity.init("gy8oodehvm");
+        ReactGA.initialize('G-J9M2G7WGCL');
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
+    return <Component {...pageProps} />
 }
